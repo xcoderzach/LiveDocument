20b11ff816fe10f7307e92a90edebad74759b529
Zach Smith <x.coder.zach@gmail.com>
2012-01-04 11:59:14 -0600

    [ZachEugene] validating length on title works

---
+++ test/validations.js
@@ -0,0 +1,46 @@
+var EventEmitter  = require("events").EventEmitter
+  , LD            = require("../index")
+  , assert        = require("assert")
+  , Mongolian     = require("mongolian")
+
+
+  , LiveDocument = LD.LiveDocument
+  , LiveDocumentMongo = LD.LiveDocumentMongo
+  , db = new Mongolian("localhost/LiveDocumentTestDB")
+
+
+var Thing = LiveDocument.define("Thing")
+  .key("title", { length: [3,24] })
+  .key("description", { max: 140, required: true })
+
+Thing.socket = new EventEmitter
+var liveDocumentMongo = new LiveDocumentMongo(new EventEmitter, db)
+
+   
+describe("LiveDocument", function() {
+  beforeEach(function(done) {
+    var socket = new EventEmitter
+    Thing.socket = socket
+    liveDocumentMongo.setSocket(socket)
+    db.collection("things").remove({}, function(err) {
+      done()
+    })
+  }) 
+  describe("with an invalid title", function() {
+    it("should not save", function(done) {
+      var thing = Thing.create({title: "a", description: "herp derp"}, function() {
+        done(new Error("Callback should not be called"))
+      })
+      thing.on("error", function(thing, invalidFields) {
+        invalidFields.should.eql({ "title": ["too short"] })
+        Thing.find({}, function(things) {
+          things.length.should.equal(0)
+          done()
+        })
+      })
+    })
+
+  })
+})
+
+
