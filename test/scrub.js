8ba550985702d7944f17fbdafba6748169e1812f
Zach Smith <x.coder.zach@gmail.com>
2012-01-12 11:38:46 -0600

    make sure it can do two fields

--- test/scrub.js
+++ test/scrub.js
@@ -4,6 +4,7 @@ var Thing = LiveDocument.define("Thing")
   .key("title", { length: [3,24] })
   .key("description", { max: 140 })
   .key("hideme", { max: 140, canRead: false})
+  .key("metoo", { max: 140, canRead: false})
  
 describe("when I scrub a document", function() {
   it("should have the non scrubbed values",function() {
@@ -13,9 +14,10 @@ describe("when I scrub a document", function() {
   })
 
   it("should not have the scrubbed values",function() {
-    var scrubbed = Thing.scrub([{title:"herp", description: "derp", hideme: "hidden"}], "read")
+    var scrubbed = Thing.scrub([{title:"herp", description: "derp", hideme: "hidden", metoo: "bye"}], "read")
     scrubbed[0].title.should.equal("herp")
     scrubbed[0].description.should.equal("derp")
     ;(typeof scrubbed[0].hideme).should.equal("undefined")
+    ;(typeof scrubbed[0].metoo).should.equal("undefined")
   })
 })
