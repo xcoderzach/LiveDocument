f0f74215991da0b07c5b464d0de892477139392e
Zach Smith <x.coder.zach@gmail.com>
2012-01-19 14:48:50 -0600

    Fixed a test that was commented out

--- test/authorization.js
+++ test/authorization.js
@@ -29,19 +29,19 @@ describe("LiveDocument", function() {
 
   describe(".editableBy method", function() { 
     describe("when given self", function() {
-    //it("should be editable by the current user", function(done) {
-    //  var currentUser = User.create({ name: "Zach Smith"
-    //                                , job: "JavaScript Developer" }, function() {
-    //    var id = currentUser.get("_id")
-    //    User.currentUserId = id
-    //    instanceLayer.setCurrentUserId(id)
-    //
-    //    User.update({_id: id}, {name: "Bizarro Zach Smith", "job": "Java Developer"}, function(user) {
-    //      user.get("name").should.equal("Bizarro Zach Smith")
-    //      done()
-    //    })
-    //  })
-    //})
+      it("should be editable by the current user", function(done) {
+        var currentUser = User.create({ name: "Zach Smith"
+                                      , job: "JavaScript Developer" }, function() {
+          var id = currentUser.get("_id")
+          User.currentUserId = id
+          instanceLayer.setCurrentUserId(id)
+      
+          User.update(id, {name: "Bizarro Zach Smith", "job": "Java Developer"}, function(user) {
+            user.get("name").should.equal("Bizarro Zach Smith")
+            done()
+          })
+        })
+      })
       it("should not be editable by other users", function(done) {
         var currentUser = User.create({ name: "Zach Smith", job: "JavaScript Developer" }, function() {
           var cantEdit = User.create({ name: "Not Zach Smith", job: "Java Developer" }, function() {
@@ -50,10 +50,8 @@ describe("LiveDocument", function() {
             User.currentUserId = currentUser.get("_id")
 
             var user = User.update(id, {name: "H4XX'd ur acct br0"}, function() {
-              assert.fail("fail")
               done("shouldn't run this, should be stopped since not authorized")
             })
-            user.herp = "derp"
             user.on("error", function(instance, message) {
               message.should.equal("Not authorized")
               done()
