dc81c59f4c2470adfefe87fd373771f96d8c96f5
Zach Smith <x.coder.zach@gmail.com>
2012-01-21 21:25:17 -0600

    embedded documents can be created (not persisted)

---
+++ test/embedded.js
@@ -0,0 +1,33 @@
+var EventEmitter      = require("events").EventEmitter
+  , LiveDocument      = require("../index")
+  , InstanceLayer     = require("../lib/drivers/mongodb/instance_layer")
+  , assert            = require("assert")
+  , Mongolian         = require("mongolian")
+  , BlogPost          = require("./models/blog_post.js")()
+  , db = new Mongolian("localhost/LiveDocumentTestDB")
+
+describe("LiveDocument", function() {
+  var instanceLayer
+  beforeEach(function(done) {
+    var socket = new EventEmitter
+
+    instanceLayer = new InstanceLayer(socket, db, __dirname + "/models")
+
+    BlogPost.setSocket(socket)
+
+    db.collection("users").remove({}, function(err) {
+      done()
+    })
+  }) 
+  describe("adding an embedded document", function() {
+    it("should have created an associated embedded document", function(done) {
+      var post = BlogPost.create({"title": "herp"}, function() {
+        post.get("comments").create({body: "Yo cool post bro"}, function(comment) {
+          post.get("comments").at(0).should.equal(comment)
+          comment.get("body").should.equal("Yo cool post bro")
+          done()
+        })
+      })
+    })
+  })
+})
