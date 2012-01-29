a8e5e24890ae033a135e9e5ed7227a4168c45f1c
Zach Smith <x.coder.zach@gmail.com>
2012-01-29 15:12:58 -0600

    added a created at timestamp

---
+++ test/timestamps.js
@@ -0,0 +1,38 @@
+var EventEmitter      = require("events").EventEmitter
+  , LiveDocument      = require("../index")
+  , InstanceLayer     = require("../lib/drivers/mongodb/instance_layer")
+  , assert            = require("assert")
+  , Mongolian         = require("mongolian")
+  , BlogPost          = require("./models/blog_post.js")()
+  , db = new Mongolian("localhost/LiveDocumentTestDB")
+
+describe("LiveDocument", function() {
+  var instanceLayer
+  beforeEach(function(done) {
+    var socket = new EventEmitter
+
+    instanceLayer = new InstanceLayer(socket, db, __dirname + "/models")
+
+    BlogPost.setSocket(socket)
+
+    db.collection("blogPosts").remove({}, function(err) {
+      try {
+        done()
+      } catch(e) {
+        console.log(e)
+      }
+    })
+  }) 
+
+  describe(".timestamps", function() {
+    it("should add createdAt when the document is created", function(done) {
+      var timeBeforeCreate = (new Date).getTime()
+      process.nextTick(function() {
+        BlogPost.create({title: "A blog post"}, function(post) {
+          post.get("createdAt").should.be.above(timeBeforeCreate)
+          done()
+        })
+      })
+    })
+  })
+})
