68f776ee0e7ea2b70209b10bee819bf80d2ecfa2
Zach Smith <x.coder.zach@gmail.com>
2012-02-19 16:49:01 -0600

    fixed associated documents

--- test/timestamps.js
+++ test/timestamps.js
@@ -1,4 +1,5 @@
 var EventEmitter      = require("events").EventEmitter
+  , timer             = require("timers")
   , LiveDocument      = require("../index")
   , InstanceLayer     = require("../lib/drivers/mongodb/instance_layer")
   , assert            = require("assert")
@@ -27,12 +28,12 @@ describe("LiveDocument", function() {
   describe(".timestamps", function() {
     it("should add createdAt when the document is created", function(done) {
       var timeBeforeCreate = (new Date).getTime()
-      process.nextTick(function() {
+      timer.setTimeout(function() {
         BlogPost.create({title: "A blog post"}, function(post) {
           post.get("createdAt").should.be.above(timeBeforeCreate)
           done()
         })
-      })
+      }, 10)
     })
     it("should add updatedAt when the document is created", function(done) {
       var timeBeforeUpdate = (new Date).getTime()
