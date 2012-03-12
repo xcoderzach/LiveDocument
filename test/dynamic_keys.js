80e5b91a2ac5b0ed532e146e35b5b7a30ba1f362
Zach Smith <x.coder.zach@gmail.com>
2012-03-11 21:49:18 -0500

    dyamic key getters

---
+++ test/dynamic_keys.js
@@ -0,0 +1,49 @@
+var EventEmitter      = require("events").EventEmitter
+  , LiveDocument      = require("../index")
+  , LiveDocumentMongo = require("../lib/drivers/mongodb/live_document_mongo")
+  , assert            = require("assert")
+  , Mongolian         = require("mongolian")
+  , db                = new Mongolian("localhost/LiveDocumentTestDB")
+  , User              = require("./models/user")
+  , Profile           = require("./models/profile")
+
+//setup the hasOne association
+
+describe("LiveDocument", function() {
+  var liveDocumentMongo
+  beforeEach(function(done) {
+    var socket = new EventEmitter
+
+    liveDocumentMongo = new LiveDocumentMongo(socket, db, __dirname + "/models")
+
+    User.setSocket(socket)
+    Profile.setSocket(socket)
+
+    db.collection("users").remove({}, function(err) {
+      db.collection("profiles").remove({}, function(err) {
+        done()
+      })
+    })
+  })
+  afterEach(function() {
+    liveDocumentMongo.cleanup()
+  }) 
+  describe("a dynamic key", function() {
+    it("should return the value on .get()", function(done) {
+      Profile.create({ first: "Zach", last: "Smith" }, function(profile) {
+        profile.get("fullName").should.equal("Zach Smith")
+        done()
+      })
+    })
+    it("should have a change notification when we change a component", function(done) {
+      Profile.create({ first: "Zach", last: "Smith" }, function(profile) {
+        profile.on("change:fullName", function() {
+          this.get("fullName").should.equal("Zack Smith")
+          done()
+        })
+        profile.set({ first: "Zack"})
+        profile.save()
+      })
+    })
+  })
+})
--- test/models/profile.js
+++ test/models/profile.js
@@ -2,5 +2,15 @@ var LiveDocument = require("../../index")
 
 var Profile = LiveDocument.define("Profile")
   .key("realName")
+  .key("first")
+  .key("last")
+  .getKey("fullName", ["first", "last"], function() {
+    return this.get("first") + " " + this.get("last")
+  })
+  .setKey("fullName", function(val) {
+    var name = val.split(" ")
+    this.set("first", name[0])
+    this.set("last", name[1])
+  })
 
 module.exports = Profile
